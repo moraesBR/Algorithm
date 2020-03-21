2607f015393ffd1fa238bae49ebf69f02822f870
Albert R. Moraes L <albert.richard@gmail.com>
2020-03-21 16:18:15 -0300

    measure the runtime in real time

--- src/sort/TestSorts.kt
+++ src/sort/TestSorts.kt
@@ -1,30 +1,60 @@
 package sort
 
+import java.util.concurrent.TimeUnit
 import kotlin.math.log10
 import kotlin.math.pow
 import kotlin.random.Random
 
-inline fun <reified T> Array<Int>.sort(sort: SortAlgorithm, noinline init: (Int) -> T): Array<Long> where T: Number, T: Comparable<T>{
+inline fun <reified T> Array<Int>.sort(nameCase: String, sort: SortAlgorithm, noinline init: (Int) -> T): Array<Long>
+        where T: Number, T: Comparable<T>
+{
+    println("Starting ${sort.getName()} for $nameCase")
+
+    val start = System.currentTimeMillis()
     val arr = Array(this.size){ 0L }
+
     this.forEach {
         val vector = Array(it, init)
         val index = log10(it.toDouble()).toInt() - 1
-        arr[index] = sort.sort(vector)
+        arr[index] = measureTimeMillis(sort,it) { sort.sort(vector) }
     }
+
+    println("Runtime(${sort.getName()}) = ${getTime(System.currentTimeMillis() - start)}\n")
+
     return arr
 }
 
-fun main(){
+inline fun measureTimeMillis(typeSort:SortAlgorithm, size: Int, block: () -> Long): Long {
 
-    val n = 4
+    print("Executing ${typeSort.getName()} for n = $size ... ")
+
+    val start = System.currentTimeMillis()
+    val out = block()
+
+    println("finished in ${getTime(System.currentTimeMillis() - start)}")
+
+    return out
+}
+
+
+fun getTime(millis: Long): String = String
+    .format("%02d:%02d:%02d:%04d",
+        TimeUnit.MILLISECONDS.toHours(millis),
+        TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+        TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)),
+        millis - TimeUnit.MILLISECONDS.toSeconds(TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)))
+        )
+
+
+fun main(){
+    val n = 6
     val nSizes = Array(n){ (10.toFloat()).pow(it+1).toInt() }
 
-    val worstCase = nSizes.sort(InsertionSort){pos -> (nSizes.size - pos).toLong() }
-    val bestCase = nSizes.sort(InsertionSort){num:Int -> num + 1L}
-    val randomCase = nSizes.sort(InsertionSort){ Random.nextLong(nSizes.size.toLong()) }
+    val bestCase = nSizes.sort("best case",InsertionSort){num:Int -> num + 1L}
+    val randomCase = nSizes.sort("random case",InsertionSort){ Random.nextLong(nSizes.size.toLong()) }
+    val worstCase = nSizes.sort("worst case",InsertionSort){pos -> (nSizes.size - pos).toLong() }
 
-    println("Best case: ${bestCase.toList()}")
-    println("Random case: ${randomCase.toList()}")
-    println("Worst case: ${worstCase.toList()}")
-
+    println("The best cases has taken ${bestCase.toList()} steps")
+    println("The random cases has took ${randomCase.toList()} steps")
+    println("the worst cases has took ${worstCase.toList()} steps")
 }
